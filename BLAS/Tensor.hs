868cf97b820a29c1d330979664d83159e184d3e2
Patrick Perry <patperry@stanford.edu>
2008-05-25 19:14:40 -0700

    BLAS/Tensor.hs: initial import
    darcs-hash:20080526021440-cb512-2d243cab29bb68bc7677b9b677a8a3cbf0f79219.gz

---
+++ BLAS/Tensor.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Tensor
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Tensor (
+    module BLAS.Tensor.Base,
+    module BLAS.Tensor.Immutable,
+    module BLAS.Tensor.ReadOnly,
+    module BLAS.Tensor.Mutable
+    ) where
+
+import BLAS.Tensor.Base
+import BLAS.Tensor.Immutable
+import BLAS.Tensor.ReadOnly
+import BLAS.Tensor.Mutable
