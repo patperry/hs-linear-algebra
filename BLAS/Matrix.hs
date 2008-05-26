8ded4143f99c37797651b7c03ce7ad84a9eb6bc3
Patrick Perry <patperry@stanford.edu>
2008-05-26 13:28:26 -0700

    BLAS/Matrix.hs: initial version
    darcs-hash:20080526202826-cb512-24afa3e5a83bdf013372da99e338a3f04eff0460.gz

---
+++ BLAS/Matrix.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Matrix
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Matrix (
+    Matrix(..)
+    ) where
+
+import BLAS.Elem.Base ( Elem )
+
+-- | A class for matrices.
+class Matrix a where
+    -- | The number of rows in the matrix.
+    numRows :: a (m,n) e -> Int
+    
+    -- | The number of columns in the matrix.
+    numCols :: a (m,n) e -> Int
+    
+    -- | Creates a new matrix view that conjugates and transposes the 
+    -- given matrix.
+    herm :: Elem e => a (m,n) e -> a (n,m) e
