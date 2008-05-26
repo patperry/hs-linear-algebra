c7c5bfb8173a3775d98579aa1135e5e82bccc47a
Patrick Perry <patperry@stanford.edu>
2008-05-25 18:22:24 -0700

    BLAS/C.hs: initial import
    darcs-hash:20080526012224-cb512-f6a0cbdd2cdfeba57bfc3c32bb7d0255e543d66c.gz

---
+++ BLAS/C.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.C
+-- Copyright  : Copyright (c) 2008, Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.C (
+    module BLAS.C.Types,
+    module BLAS.C.Level1,
+    module BLAS.C.Level2,
+    module BLAS.C.Level3,
+    ) where
+        
+import BLAS.C.Types
+import BLAS.C.Level1
+import BLAS.C.Level2
+import BLAS.C.Level3
