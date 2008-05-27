00fb88c31a1e0c6153d6657652ec562f9c3330c4
Patrick Perry <patperry@stanford.edu>
2008-05-26 23:51:00 -0700

    BLAS/Matrix/Immutable.hs: initial version
    darcs-hash:20080527065100-cb512-a098f0ab74d62b4f25e276c6d74a2592ee070293.gz

---
+++ BLAS/Matrix/Immutable.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Matrix.Immutable
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Matrix.Immutable (
+    IMatrix(..)
+    ) where
+
+import BLAS.Access
+import BLAS.Elem ( BLAS3 )
+import qualified BLAS.Matrix.Base as Base
+import Data.Vector.Dense
+import Data.Matrix.Dense.Internal
+import Data.Matrix.Dense.Operations ( apply, applyMat )
+
+infixl 7 <*>
+
+class Base.Matrix a => IMatrix a e where
+    -- | Apply to a vector
+    (<*>) :: a (m,n) e -> Vector n e -> Vector m e
+    
+    -- | Apply to a matrix
+    (<>) :: a (m,k) e -> Matrix (k,n) e -> Matrix (m,n) e
+
+instance (BLAS3 e) => IMatrix (DMatrix Imm) e where
+    (<*>) = apply
+    (<>)  = applyMat
+    
