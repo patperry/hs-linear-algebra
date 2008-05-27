76ade127eb67195df80ed16bfb1195130d7ceac5
Patrick Perry <patperry@stanford.edu>
2008-05-27 13:37:43 -0700

    BLAS/Matrix/Solve/ReadOnly.hs: initial version
    darcs-hash:20080527203743-cb512-657105d6f6100efb51661656c24315153665c9e6.gz

---
+++ BLAS/Matrix/Solve/ReadOnly.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Matrix.Solve.ReadOnly
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Matrix.Solve.ReadOnly (
+    RSolve(..)
+    ) where
+
+import BLAS.Matrix.ReadOnly
+
+import Data.Vector.Dense.IO ( DVector )
+import Data.Matrix.Dense.IO ( DMatrix )
+
+class RMatrix a e => RSolve a e where
+    -- | Solve for a vector
+    getSolve :: a (m,n) e -> DVector t m e -> DVector r n e
+    
+    -- | Solve for a matrix
+    getSolveMat :: a (m,k) e -> DMatrix t (m,n) e -> DMatrix r (k,n) e
+
