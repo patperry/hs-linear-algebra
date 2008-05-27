1210459a4843fd5fa0d434ad03b004880e48de89
Patrick Perry <patperry@stanford.edu>
2008-05-27 00:15:37 -0700

    BLAS/Matrix/ReadOnly.hs: initial version
    darcs-hash:20080527071537-cb512-6cceb61948b1f8857567f2b7ddf7eceab573319e.gz

---
+++ BLAS/Matrix/ReadOnly.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Matrix.ReadOnly
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Matrix.ReadOnly (
+    RMatrix(..)
+    ) where
+
+import BLAS.Elem ( BLAS3 )
+import qualified BLAS.Matrix.Base as Base
+import Data.Vector.Dense.Internal
+import Data.Matrix.Dense.Internal
+import qualified Data.Matrix.Dense.Operations as M
+
+class Base.Matrix a => RMatrix a e where
+    -- | Apply to a vector
+    getApply :: a (m,n) e -> DVector t n e -> IO (DVector r m e)
+    
+    -- | Apply to a matrix
+    getApplyMat :: a (m,k) e -> DMatrix t (k,n) e -> IO (DMatrix r (m,n) e)
+
+instance (BLAS3 e) => RMatrix (DMatrix t) e where
+    getApply    = M.getApply
+    getApplyMat = M.getApplyMat
