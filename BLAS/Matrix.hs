069421a4cc1de45b2fa4b811c7824a263f87afd7
Patrick Perry <patperry@stanford.edu>
2008-05-27 00:18:48 -0700

    BLAS/Matrix.hs: initial version
    darcs-hash:20080527071848-cb512-2a37c80b5992c8748c67e4c9f8130372e60f7e62.gz

---
+++ BLAS/Matrix.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Matrix
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Matrix (
+    module BLAS.Matrix.Base,
+    module BLAS.Matrix.Immutable,
+    module BLAS.Matrix.ReadOnly,
+    ) where
+
+import BLAS.Matrix.Base
+import BLAS.Matrix.Immutable
+import BLAS.Matrix.ReadOnly
