fc96d3b5e60b70aec5d54b372b3244428f8ece6d
Patrick Perry <patperry@stanford.edu>
2008-05-27 10:32:03 -0700

    Data/Matrix/Tri.hs: initial version
    darcs-hash:20080527173203-cb512-88a5de056d4c7c500c24808edb73f0f59c3f399d.gz

---
+++ Data/Matrix/Tri.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Data.Matrix.Tri
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module Data.Matrix.Tri (
+    Tri(..),
+    UpLo(..), Diag(..),
+
+    fromBase,
+    toBase,
+    mapTri,
+
+    ) where
+
+import qualified BLAS.Elem as E
+import BLAS.Matrix
+import BLAS.Tensor
+import BLAS.Types ( UpLo(..), Diag(..) )
+
+data Tri a nn e = Tri UpLo Diag e (a nn e)
+
+mapTri :: (a (n,n) e -> b (n,n) e) -> Tri a (n,n) e -> Tri b (n,n) e
+mapTri f (Tri u d n a) = Tri u d n $ f a
+
+fromBase :: UpLo -> Diag -> e -> a (n,n) e -> Tri a (n,n) e
+fromBase = Tri
+        
+toBase :: Tri a (n,n) e -> (UpLo, Diag, e, a (n,n) e)
+toBase (Tri u d e a) = (u,d,e,a)
+
+instance Matrix a => Matrix (Tri a) where
+    numRows (Tri _ _ _ a) = numRows a
+    numCols (Tri _ _ _ a) = numCols a
+    herm    (Tri u d e a) = Tri u d (E.conj e) (herm a)
+    
+instance (Num e) => Scalable (Tri a nn) e where
+    (*>) k (Tri u d e a) = Tri u d (k*e) a
