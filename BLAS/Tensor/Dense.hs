7904d4f5f5658f19dea659ac90ae3b60649e00a7
Patrick Perry <patperry@stanford.edu>
2008-05-28 15:39:19 -0700

    BLAS/Tensor/Dense.hs: initial version
    darcs-hash:20080528223919-cb512-28437cff459d917c92897557ddcc66b8989005a9.gz

---
+++ BLAS/Tensor/Dense.hs
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : BLAS.Tensor.Dense
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module BLAS.Tensor.Dense (
+    module BLAS.Tensor.Dense.Immutable,
+    module BLAS.Tensor.Dense.ReadOnly,
+    ) where
+
+import BLAS.Tensor.Dense.Immutable
+import BLAS.Tensor.Dense.ReadOnly
