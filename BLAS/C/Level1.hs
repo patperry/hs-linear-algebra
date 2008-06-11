c5fa752d860eb736d08b05806e6d66c67c124d63
Patrick Perry <patperry@stanford.edu>
2008-06-11 00:04:02 -0700

    BLAS/C/Level1.hs: add -fno-excess-precision flag for portability of acxpy implementation.
    darcs-hash:20080611070402-cb512-ec4215af77097348365a10fdad1a500124afde7c.gz

--- BLAS/C/Level1.hs
+++ BLAS/C/Level1.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-excess-precision #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module     : BLAS.C.Level1
