cd2c4105e63fa9467c3cb9204ffdab030004c753
Patrick Perry <patperry@stanford.edu>
2008-07-08 15:26:09 -0700

    Data/Matrix/Banded/IO.hs: initial version
    darcs-hash:20080708222609-cb512-2f6ba391d6c62f68b08467b0e649de84466d3f6c.gz

---
+++ Data/Matrix/Banded/IO.hs
@@ -0,0 +1,81 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Data.Matrix.Banded.IO
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module Data.Matrix.Banded.IO (
+    -- * The mutable banded matrix data type
+    BMatrix(..),
+    IOBanded,
+    
+    module BLAS.Matrix.Base,
+    module BLAS.Matrix.ReadOnly,
+    module BLAS.Tensor.Base,
+    module BLAS.Tensor.Dense.ReadOnly,
+    module BLAS.Tensor.ReadOnly,
+    module BLAS.Tensor.Mutable,
+    
+    -- * Creating new matrices
+    newBanded,
+    newBanded_,
+    newListsBanded,
+
+    -- * Views
+    -- ** Rows and columns
+    rowView,
+    colView,
+    getRow,
+    getCol,
+    
+    -- ** Diagonals
+    diag,
+    
+    -- * Operations
+    -- module Data.Matrix.Dense.Operations,
+    
+    -- * Converting to and from banded matrices
+    -- ** @ForeignPtr@s
+    toForeignPtr,
+    fromForeignPtr,
+    ldaOf,
+    isHerm,
+
+    -- ** Dense matrices
+    toMatrix,
+    fromMatrix,
+    
+    -- * Bandwith properties
+    bandwidth,
+    numLower,
+    numUpper,
+    
+    -- * Coercing
+    coerceMatrix,
+    
+    -- * Unsafe operations
+    unsafeNewBanded,
+    unsafeWithElemPtr,
+    unsafeRowView,
+    unsafeColView,
+    unsafeGetRow,
+    unsafeGetCol,
+    unsafeDiag,
+    unsafeFreeze,
+    unsafeThaw,
+    
+    ) where
+
+import Data.Matrix.Banded.Internal
+-- import Data.Matrix.Banded.Operations hiding ( gemv, gemm, apply, applyMat,
+--    sapply, sapplyMat, add, plus, minus, times, divide, getApply, getApplyMat )
+    
+import BLAS.Matrix.Base hiding ( Matrix )
+import BLAS.Matrix.ReadOnly
+import BLAS.Tensor.Base
+import BLAS.Tensor.Dense.ReadOnly
+import BLAS.Tensor.ReadOnly
+import BLAS.Tensor.Mutable
