ea4b3370451b72871e2fdf5911fd29b2cbf3546b
Patrick Perry <patperry@stanford.edu>
2008-07-27 15:58:26 -0700

    Data/Matrix/Herm/Bandes.hs: initial version
    darcs-hash:20080727225826-cb512-6cbbbe6e606dae6cf9d841ce1a6a7a08b93dff91.gz

---
+++ Data/Matrix/Herm/Banded.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Data.Matrix.Herm.Banded
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module Data.Matrix.Herm.Banded (
+    module Data.Matrix.Herm,
+    module BLAS.Matrix.Immutable,
+    module BLAS.Matrix.ReadOnly,
+
+    hbmv,
+    hbmm,
+    ) where
+
+import Control.Monad ( zipWithM_ )
+import System.IO.Unsafe
+import Unsafe.Coerce
+
+import BLAS.Access
+import BLAS.Elem ( BLAS2 )
+import BLAS.C ( colMajor, cblasUpLo )
+import BLAS.Types ( flipUpLo )
+import qualified BLAS.Elem as E
+import qualified BLAS.C as BLAS
+
+import Data.Matrix.Banded.Internal
+import Data.Matrix.Dense.Internal ( DMatrix, IOMatrix )
+import Data.Vector.Dense.Internal
+import qualified Data.Matrix.Banded.Internal as B
+import qualified Data.Matrix.Dense.Internal as M
+import qualified Data.Vector.Dense.Internal as V
+import qualified Data.Vector.Dense.Operations as V
+
+import Data.Matrix.Herm
+import BLAS.Matrix.Immutable
+import BLAS.Matrix.ReadOnly
+
+
+instance (BLAS2 e) => IMatrix (Herm (BMatrix Imm)) e where
+    (<*>) h x = unsafePerformIO $ getApply h x
+    {-# NOINLINE (<*>) #-}
+
+    (<**>) h a = unsafePerformIO $ getApplyMat h a
+    {-# NOINLINE (<**>) #-}
+
+
+instance (BLAS2 e) => RMatrix (Herm (BMatrix s)) e where
+    getApply h x = do
+        y <- newZero (dim x)
+        hbmv 1 (unsafeCoerce h) x 1 y
+        return (unsafeCoerce y)
+    
+    getApplyMat h a = do
+        b <- newZero (shape a)
+        hbmm 1 (unsafeCoerce h) a 1 b
+        return (unsafeCoerce b)
+
+
+hbmv :: (BLAS2 e) => e -> Herm (BMatrix t) (n,n) e -> DVector s n e -> e -> IOVector n e -> IO ()
+hbmv alpha h x beta y
+    | numRows h == 0 =
+        return ()
+    | isConj y = do
+        V.doConj y
+        hbmv alpha h x beta (V.conj y)
+        V.doConj y
+    | isConj x = do
+        x' <- newCopy x
+        V.doConj (V.unsafeThaw x')
+        hbmv alpha h (conj x') beta y
+    | otherwise =
+        let order   = colMajor
+            (u,e,a) = toBase h
+            alpha'  = alpha * e
+            n       = numCols a
+            k       = case u of 
+                          Upper -> numUpper a
+                          Lower -> numLower a      
+            (u',alpha'') 
+                    = case (isHerm a) of
+                          True  -> (flipUpLo u, E.conj alpha')
+                          False -> (u, alpha')
+            uploA   = cblasUpLo u'
+            ldA     = ldaOf a
+            incX    = strideOf x
+            incY    = strideOf y
+        in B.unsafeWithBasePtr a  $ \pA ->
+               V.unsafeWithElemPtr x 0 $ \pX ->
+                    V.unsafeWithElemPtr y 0 $ \pY -> do
+                        BLAS.hbmv order uploA n k alpha'' pA ldA pX incX beta pY incY
+
+hbmm :: (BLAS2 e) => e -> Herm (BMatrix t) (m,m) e -> DMatrix s (m,n) e -> e -> IOMatrix (m,n) e -> IO ()
+hbmm = undefined {- alpha h b beta c
+    | numRows b == 0 || numCols b == 0 || numCols c == 0 = return ()
+    | (isHerm a) /= (isHerm c) || (isHerm a) /= (isHerm b) =
+        zipWithM_ (\x y -> hemv alpha h x beta y) (cols b) (cols c)
+    | otherwise =
+        let order   = colMajor
+            (m,n)   = shape c
+            alpha'  = alpha * e
+            (side,u',m',n', alpha'')
+                    = if isHerm a
+                          then (rightSide, flipUpLo u, n, m, E.conj alpha')
+                          else (leftSide,  u, m, n, alpha')
+            uploA   = cblasUpLo u'
+            ldA     = ldaOf a
+            ldB     = ldaOf b
+            ldC     = ldaOf c
+        in M.unsafeWithElemPtr a (0,0) $ \pA ->
+               M.unsafeWithElemPtr b (0,0) $ \pB ->
+                   M.unsafeWithElemPtr c (0,0) $ \pC ->
+                       BLAS.hemm order side uploA m' n' alpha'' pA ldA pB ldB beta pC ldC
+    where
+      (u,e,a) = toBase h -}
