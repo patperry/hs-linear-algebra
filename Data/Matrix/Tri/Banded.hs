6479a8fcce668aa06ed3fb1a2064fb1a6502a0e4
Patrick Perry <patperry@stanford.edu>
2008-07-28 12:38:28 -0700

    Data/Matrix/Tri/Banded.hs: implement tbmv
    darcs-hash:20080728193828-cb512-225aafff2e34eef60f07132e48c6ebf2cd6db031.gz

--- Data/Matrix/Tri/Banded.hs
+++ Data/Matrix/Tri/Banded.hs
@@ -20,25 +20,26 @@ module Data.Matrix.Tri.Banded (
     tbsm
     ) where
 
--- import Control.Monad ( when )
+import Control.Monad ( when )
 import System.IO.Unsafe
 import Unsafe.Coerce
 
 import Data.Matrix.Banded.Internal
+import qualified Data.Matrix.Banded.Internal as B
 import Data.Matrix.Dense.Internal ( IOMatrix )
 import qualified Data.Matrix.Dense.Internal as M
 import Data.Vector.Dense.Internal
 import qualified Data.Vector.Dense.Internal as V
--- import qualified Data.Vector.Dense.Operations as V
+import qualified Data.Vector.Dense.Operations as V
 
 import BLAS.Access
 import BLAS.Elem ( BLAS2 )
 -- import qualified BLAS.Elem as E
--- import BLAS.C.Types ( cblasDiag, cblasUpLo, cblasTrans, colMajor, 
---    noTrans, conjTrans, leftSide, rightSide )
--- import BLAS.Types ( Trans(..), flipTrans, flipUpLo )
+import BLAS.C.Types ( cblasDiag, cblasUpLo, cblasTrans, colMajor, 
+    noTrans, conjTrans, leftSide, rightSide )
+import BLAS.Types ( Trans(..), flipTrans, flipUpLo )
                                    
--- import qualified BLAS.C as BLAS
+import qualified BLAS.C as BLAS
 -- import qualified BLAS.C.Types as BLAS
 
 import BLAS.Matrix.Immutable
@@ -85,20 +86,30 @@ instance (BLAS2 e) => RSolve (Tri (BMatrix s)) e where
 
 
 tbmv :: (BLAS2 e) => Tri (BMatrix t) (n,n) e -> IOVector n e -> IO ()
-tbmv = undefined {- t x =
+tbmv t x | isConj x = do
+    V.doConj x
+    tbmv t (conj x)
+    V.doConj x
+    
+tbmv t x =
     let (u,d,alpha,a) = toBase t
         order     = colMajor
         (transA,u') = if isHerm a then (conjTrans, flipUpLo u) else (noTrans, u)
         uploA     = cblasUpLo u'
         diagA     = cblasDiag d
         n         = numCols a
+        k         = case u of Upper -> numUpper a 
+                              Lower -> numLower a
         ldA       = ldaOf a
         incX      = strideOf x
-    in M.unsafeWithElemPtr a (0,0) $ \pA ->
+        withPtrA  = case u' of 
+                        Upper -> B.unsafeWithBasePtr a
+                        Lower -> B.unsafeWithElemPtr a (0,0)
+    in withPtrA $ \pA ->
            V.unsafeWithElemPtr x 0 $ \pX -> do
-               BLAS.trmv order uploA transA diagA n pA ldA pX incX
+               BLAS.tbmv order uploA transA diagA n k pA ldA pX incX
                when (alpha /= 1) $ V.scaleBy alpha x
--}               
+              
                
 tbmm :: (BLAS2 e) => Tri (BMatrix t) (m,m) e -> IOMatrix (m,n) e -> IO ()
 tbmm t b = mapM_ (\x -> tbmv t x) (M.cols b)
