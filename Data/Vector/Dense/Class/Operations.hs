f30e0a956b34722e316847d27e1b3b2c48510a73
Patrick Perry <patperry@stanford.edu>
2008-10-31 00:21:05 -0700

    Add some unary vector ops
    darcs-hash:20081031072105-cb512-75b8f3396a3c413b25dca851bc4160dd871e0026.gz

--- Data/Vector/Dense/Class/Operations.hs
+++ Data/Vector/Dense/Class/Operations.hs
@@ -10,6 +10,9 @@
 module Data.Vector.Dense.Class.Operations (
     -- * Vector operations
     getConjVector,
+    getScaledVector,
+    getShiftedVector,
+    
     axpyVector,
     mulVector,
     divVector,
@@ -27,12 +30,28 @@ import BLAS.Numeric
 import BLAS.Tensor( shape )
 import Data.Vector.Dense.Class.Internal
 
+-- | Get a new vector with elements with the conjugates of the elements
+-- of the given vector
 getConjVector :: (ReadVector x e m, WriteVector y e m, BLAS1 e) =>
     x n e -> m (y n e)
-getConjVector x = do
-    y <- newCopyVector x
-    doConjVector y
-    return y
+getConjVector = getUnaryOp doConjVector
+{-# INLINE getConjVector #-}
+
+-- | Get a new vector by scaling the elements of another vector
+-- by a given value.
+getScaledVector :: (ReadVector x e m, WriteVector y e m, BLAS1 e) =>
+    e -> x n e -> m (y n e)
+getScaledVector e = getUnaryOp (scaleByVector e)
+{-# INLINE getScaledVector #-}
+
+-- | Get a new vector by shifting the elements of another vector
+-- by a given value.
+getShiftedVector :: (ReadVector x e m, WriteVector y e m, BLAS1 e) =>
+    e -> x n e -> m (y n e)
+getShiftedVector e = getUnaryOp (shiftByVector e)
+{-# INLINE getShiftedVector #-}
+
+
 
 axpyVector :: (ReadVector x e m, WriteVector y e m, BLAS1 e) =>
     e -> x n e -> y n e -> m ()
@@ -51,3 +70,13 @@ divVector :: (WriteVector y e m, ReadVector x e m, BLAS1 e) =>
 divVector y x =
     checkBinaryOp (shape y) (shape x) $ unsafeDivVector y x
 {-# INLINE divVector #-}
+
+
+getUnaryOp :: (ReadVector x e m, WriteVector y e m, BLAS1 e) =>
+    (y n e -> m ()) -> x n e -> m (y n e)
+getUnaryOp f x = do
+    y <- newCopyVector x
+    f y
+    return y
+{-# INLINE getUnaryOp #-}
+
