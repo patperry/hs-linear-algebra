89a69b358dee18c2fb989978634c15d2587f597a
Patrick Perry <patperry@stanford.edu>
2008-11-01 01:42:01 -0700

    Cabal fix
    darcs-hash:20081101084201-cb512-c5ff2283f307272320ab8d4b574f0b16089f4016.gz

---
+++ examples/LU.hs
@@ -0,0 +1,65 @@
+
+module LU ( luFactorize ) where
+
+import BLAS.Elem( BLAS3 )
+
+import Control.Monad
+import Control.Monad.ST
+
+import Data.Matrix.Dense
+import Data.Matrix.Dense.ST
+import Data.Matrix.Tri
+import Data.Vector.Dense.ST
+
+
+lu :: (BLAS3 e) => Matrix (m,n) e -> Either Int (Matrix (m,n) e, [Int])
+lu a = runST $ do
+    a' <- thawMatrix a
+    luFactorize a' >>=
+        return . either Left (\pivots ->
+            Right (unsafeFreezeMatrix a', pivots)
+            )
+
+
+{-
+ - Recursive LU factorization with row pivoting.  Takes a matrix
+ - A and factors it as P A = L U, where P is a permutation matrix, 
+ - L is a lower triangular matrix with ones along the diagonal, and 
+ - U is an upper triangular matrix.  On successful return, the values of
+ - L and U are stored in A, and a list of the row swaps are returned.
+ - On failure, the index of the failing column is returned.
+ -}      
+luFactorize :: (BLAS3 e) => STMatrix s (m,n) e -> ST s (Either Int [Int])
+luFactorize a
+    | mn > 1 =
+        let nleft = mn `div` 2
+            (a_1, a_2) = splitColsAt nleft a
+            (a11, a21) = splitRowsAt nleft a_1
+            (a12, a22) = splitRowsAt nleft a_2
+        in luFactorize a_1 >>=
+               either (return . Left) (\pivots -> do
+                   zipWithM_ (swapRows a_2) [0..] pivots
+                   doSolveMat_ (lowerU a11) a12
+                   doSApplyAddMat (-1) a21 a12 1 a22
+                   luFactorize a22 >>=
+                       either (return . Left . (nleft+)) (\pivots' -> do
+                           zipWithM_ (swapRows a21) [0..] pivots'
+                           return (Right $ pivots ++ map (nleft+) pivots')
+                       )
+               )
+    | mn == 1 = 
+        let x = colView a 0
+        in getWhichMaxAbs x >>= \(i,e) ->
+            if (e /= 0) 
+                then do
+                    scaleBy (1/e) x
+                    readElem x 0 >>= writeElem x i
+                    writeElem x 0 e
+                    return $ Right [i]
+                else
+                    return $ Left 0
+    | otherwise =
+        return (Right [])
+  where
+    (m,n) = shape a
+    mn    = min m n
