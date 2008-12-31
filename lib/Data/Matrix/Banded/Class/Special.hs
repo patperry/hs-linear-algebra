a48d7156cd3a9c9dd891bae6a11ae11c91a6cbfc
Patrick Perry <patperry@stanford.edu>
2008-12-31 09:44:45 -0800

    Add missing file to repo
    darcs-hash:20081231174445-cb512-60ff5bd59e8a0ef5c7f87d9fc59dee8d2d41acf3.gz

---
+++ lib/Data/Matrix/Banded/Class/Special.hs
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Data.Matrix.Banded.Class.Special
+-- Copyright  : Copyright (c) , Patrick Perry <patperry@stanford.edu>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@stanford.edu>
+-- Stability  : experimental
+--
+
+module Data.Matrix.Banded.Class.Special (
+    -- * Special banded matrices
+    newZeroBanded,
+    setZeroBanded,
+    newConstantBanded,
+    setConstantBanded,
+    ) where
+
+import Data.Matrix.Banded.Class.Internal
+
