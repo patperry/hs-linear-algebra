587cb55eaef9db3a377e2fde154399cad1c4a7f2
Patrick Perry <patperry@stanford.edu>
2009-01-21 13:46:28 -0800

    add test coverage for trmv/trmm

--- tests/TriMatrix.hs
+++ tests/TriMatrix.hs
@@ -11,11 +11,14 @@ module TriMatrix
     where
 
 import Driver
+import Monadic
 import Test.Matrix.Tri.Dense
 import qualified Test.QuickCheck.BLAS as Test
 
 import Data.Vector.Dense
+import Data.Vector.Dense.ST
 import Data.Matrix.Dense
+import Data.Matrix.Dense.ST
 import Data.Matrix.Tri
 
 type V = Vector Int E
@@ -36,11 +39,25 @@ prop_tri_apply (TriMatrixMV (t :: TM) a x) =
 prop_tri_sapply k (TriMatrixMV (t :: TM) a x) =
     sapplyVector k t x ~== sapplyVector k a x
 
+prop_doSapplyAddVector alpha beta (TriMatrixMV (a :: TM) _ x) = monadicST $ do
+    forAllM (Test.vector (numRows a)) $ \y -> do
+        y'  <- run $ unsafeThawVector y
+        y'' <- run $ freezeVector y'
+        run $ doSApplyAddVector alpha a x beta y'
+        assert $ y ~== a <*> (alpha *> x) + (beta *> y'')
+
 prop_tri_applyMatrix (TriMatrixMM (t :: TM) a b) =
     t <**> b ~== a <**> b
 
 prop_tri_sapplyMatrix k (TriMatrixMM (t :: TM) a b) =
     sapplyMatrix k t b ~== sapplyMatrix k a b
+
+prop_doSapplyAddMatrix alpha beta (TriMatrixMM (a :: TM) _ b) = monadicST $ do
+    forAllM (Test.matrix (numRows a, numCols b)) $ \c -> do
+        c'  <- run $ unsafeThawMatrix c
+        c'' <- run $ freezeMatrix c'
+        run $ doSApplyAddMatrix alpha a b beta c'
+        assert $ c ~== a <**> (alpha *> b) + (beta *> c'')
 
 prop_tri_solve (TriMatrixSV (t :: TM) y) =
     let x = t <\> y
@@ -62,8 +79,10 @@ tests_TriMatrix =
     , ("tri row"               , mytest prop_tri_row)
     , ("tri apply"             , mytest prop_tri_apply)
     , ("tri sapply"            , mytest prop_tri_sapply)
+    , ("tri doSApplyAddVector"     , mytest prop_doSapplyAddVector)    
     , ("tri applyMatrix"          , mytest prop_tri_applyMatrix)
     , ("tri sapplyMatrix"         , mytest prop_tri_sapplyMatrix)
+    , ("tri doSApplyAddMatrix"     , mytest prop_doSapplyAddMatrix)        
 
     , ("tri solve"             , mytest prop_tri_solve)
     , ("tri ssolve"            , mytest prop_tri_ssolve)
