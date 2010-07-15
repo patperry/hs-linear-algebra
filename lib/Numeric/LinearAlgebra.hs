fd2c35226721f9e57038d68a368503d65673569a
Patrick Perry <patperry@gmail.com>
2010-07-15 11:53:12 -0400

    Add Numeric.LinearAlgebra module which re-exports submodules

---
+++ lib/Numeric/LinearAlgebra.hs
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module     : Numeric.LinearAlgebra
+-- Copyright  : Copyright (c) 2010, Patrick Perry <patperry@gmail.com>
+-- License    : BSD3
+-- Maintainer : Patrick Perry <patperry@gmail.com>
+-- Stability  : experimental
+--
+-- Linear algebra types and operations
+--
+
+module Numeric.LinearAlgebra (
+    module Numeric.LinearAlgebra.Elem,
+    module Numeric.LinearAlgebra.Vector,
+    module Numeric.LinearAlgebra.Vector.ST,
+    module Numeric.LinearAlgebra.Matrix,
+    module Numeric.LinearAlgebra.Matrix.ST,
+    ) where
+
+import Numeric.LinearAlgebra.Elem
+import Numeric.LinearAlgebra.Vector
+import Numeric.LinearAlgebra.Vector.ST
+import Numeric.LinearAlgebra.Matrix
+import Numeric.LinearAlgebra.Matrix.ST
+
