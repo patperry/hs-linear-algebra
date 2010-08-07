1e004900c3bb0db230e68b00bb5b532f27c671ec
Patrick Perry <patperry@gmail.com>
2010-08-07 14:06:13 -0400

    Skeleton implementation of Cholesky module

--- lib/Numeric/LinearAlgebra/Factor/Cholesky.hs
+++ lib/Numeric/LinearAlgebra/Factor/Cholesky.hs
@@ -10,39 +10,62 @@
 -- matrices.
 --
 
+module Numeric.LinearAlgebra.Factor.Cholesky (
+    Chol(..),
+    cholFactorHermMatrix,
+    cholFactorToHermMatrix,
+    cholMatrixSolveVector,
+    cholMatrixSolveToVector,
+    cholMatrixSolveMatrix,
+    cholMatrixSolveToMatrix,
+    ) where
+
+import Control.Monad.ST( ST )
+
+-- import qualified Foreign.LAPACK as LAPACK
+
+import Numeric.LinearAlgebra.Types
+import Numeric.LinearAlgebra.Matrix
+import Numeric.LinearAlgebra.Matrix.Herm
+import Numeric.LinearAlgebra.Matrix.ST
+import Numeric.LinearAlgebra.Vector
+import Numeric.LinearAlgebra.Vector.ST
+
 -- | A Cholesky decomposition view of a matrix.
 data Chol m e = Chol Uplo (m e) deriving (Show)
-
-class LAPACK e where
 
 cholFactorHermMatrix :: (LAPACK e)
                      => Herm Matrix e
                      -> ST s (Either Int (Chol Matrix e))
+cholFactorHermMatrix = undefined
 
-
-cholFactorToHermMatrix :: (LAPACK e, RMatrix m)
-                       => Herm m e
-                       => Herm (STMatrix s) e
-                       => ST s (Either Int (Chol (STMatrix s) e))
+cholFactorToHermMatrix :: (LAPACK e)
+                       -> Herm (STMatrix s) e
+                       -> ST s (Either Int (Chol (STMatrix s) e))
+cholFactorToHermMatrix = undefined
 
 cholMatrixSolveVector :: (LAPACK e)
                       => Chol Matrix e
                       -> Vector e
                       -> Vector e
+cholMatrixSolveVector = undefined
 
 cholMatrixSolveMatrix :: (LAPACK e)
                       => Chol Matrix e
                       -> Matrix e
                       -> Matrix e
+cholMatrixSolveMatrix = undefined
 
 cholMatrixSolveToVector :: (LAPACK e, RMatrix m, RVector v)
-                        -> Chol m e
+                        => Chol m e
                         -> v e
                         -> STVector s e
                         -> ST s ()
+cholMatrixSolveToVector = undefined
 
-cholMatrixSolveToMatrix :: (LAPACK e, RMatrix m1 RMatrix m2)
-                        -> Chol m1 e
+cholMatrixSolveToMatrix = undefined
+cholMatrixSolveToMatrix :: (LAPACK e, RMatrix m1, RMatrix m2)
+                        => Chol m1 e
                         -> m2 e
                         -> STMatrix s e
                         -> ST s ()
